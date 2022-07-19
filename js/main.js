a448d3083ef93b4dedff50a9f534ac9211c03d51
Marco Valerio <mo.valerio93@gmal.com>
2022-07-19 18:48:52 +0200

    our tea add img complited and fixed

--- js/main.js
+++ js/main.js
@@ -3,47 +3,66 @@ const team = [
     {
         'name':'Wayne Barnett',
         'role':'Founder & CEO',
-        'img':'wayne-barnett-founder-ceo.jpg'
+        'image':'wayne-barnett-founder-ceo.jpg'
     },
     {
         'name':'Angela Caroll',
         'role':'Chief Editor',
-        'img':'angela-caroll-chief-editor.jpg'
+        'image':'angela-caroll-chief-editor.jpg'
     },
     {
         'name':'Walter Gordon',
         'role':'Office Manager',
-        'img':'walter-gordon-office-manager.jpg'
+        'image':'walter-gordon-office-manager.jpg'
     },
     {
         'name':'Angela Lopez',
         'role':'Social Media Manager',
-        'img':'angela-lopez-social-media-manager.jpg'
+        'image':'angela-lopez-social-media-manager.jpg'
     },
     {
         'name':'Scott Estrada',
         'role':'Developer',
-        'img':'scott-estrada-developer.jpg'
+        'image':'scott-estrada-developer.jpg'
     },
     {
         'name':'Barbara Ramos',
         'role':'Graphic Designer',
-        'img':'barbara-ramos-graphic-designer.jpg'
+        'image':'barbara-ramos-graphic-designer.jpg'
     }
 ];
 
-const teamDom
+const teamDom= document.querySelector('.team')
 
 
 //stampo in console oggetti 
 for (let i = 0; i < team.length; i ++) {
-    console.log(team[i]['name'], team[i]['role'], team[i]['img']);
+
+    console.log(team[i]['name'], team[i]['role'], team[i]['image']);
+
     drawTeamMembers(team[i]);
 
 }
+
 //stampo info in html
-function drawTeamMembers(teamMeber) [
-    const card = docoument.createElement('div');
-    card.classlst.add('card');
-    
-]
+function drawTeamMembers (teamMember) {
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const img = document.createElement('img'); 
+
+    img.src = `./img/${teamMember.image}`;
+
+    const h2 = document.createElement('h2');
+    h2.innerText = teamMember.name;
+
+    const h3 = document.createElement('h3');
+    h3.innerText = teamMember.role ;
+
+    card.append(img);
+    card.append(h2);
+    card.append(h3);
+
+    teamDom.append(card);    
+}
